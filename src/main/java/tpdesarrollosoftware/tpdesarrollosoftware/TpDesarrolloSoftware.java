72cea7fcf6d8a266af3b0d2c7bb9a3b21d43b70c
alerlk0912 <arruaalejandro0@gmail.com>
2024-08-27 08:34:29 -0300

    1

---
+++ src/main/java/tpdesarrollosoftware/tpdesarrollosoftware/TpDesarrolloSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tpdesarrollosoftware.tpdesarrollosoftware;
+
+/**
+ *
+ * @author Ale
+ */
+public class TpDesarrolloSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
