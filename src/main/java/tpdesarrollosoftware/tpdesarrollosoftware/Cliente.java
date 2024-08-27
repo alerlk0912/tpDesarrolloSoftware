1694db001a467904aba9e548ec3af23ba06d3125
francoycc <francoycc@gmail.com>
2024-08-27 10:26:48 -0300

    ´probandoooo

---
+++ src/main/java/tpdesarrollosoftware/tpdesarrollosoftware/Cliente.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpdesarrollosoftware.tpdesarrollosoftware;
+
+/**
+ *
+ * @author Ale
+ */
+public class Cliente {
+    private String nombre;
+    private int id;
+    private Coordenada ubicacion;
+
+    public Cliente(String nombre, int id, Coordenada ubicacion) {
+        this.nombre = nombre;
+        this.id = id;
+        this.ubicacion = ubicacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Coordenada getUbicacion() {
+        return ubicacion;
+    }
+}
---
+++ src/main/java/tpdesarrollosoftware/tpdesarrollosoftware/Coordenada.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpdesarrollosoftware.tpdesarrollosoftware;
+
+/**
+ *
+ * @author Ale
+ */
+public class Coordenada {
+    private double latitud;
+    private double longitud;
+
+    public Coordenada(double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+}
