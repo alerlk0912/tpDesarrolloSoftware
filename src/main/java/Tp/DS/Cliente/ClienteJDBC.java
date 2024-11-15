d6e6006359049f77320f116de2f7fa486b1bfc3b
francoycc <francoycc@gmail.com>
2024-11-15 13:33:44 -0300

    Etapa 7 en Proceso - Anda crear cliente en JDBC y cargar la tabla

--- src/main/java/Tp/DS/Cliente/ClienteJDBC.java
+++ src/main/java/Tp/DS/Cliente/ClienteJDBC.java
@@ -19,7 +19,7 @@ public class ClienteJDBC implements DAOCliente {
 
     @Override
     public void actualizarCliente(Cliente cliente){
-        String sql = "UPDATE clientes SET cuit = ?, nombre = ?, email = ?, direccion = ?, coordenadas = ? WHERE id = ?";
+        String sql = "UPDATE cliente SET cuit = ?, nombre = ?, email = ?, direccion = ?, coordenadas = ? WHERE id = ?";
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setString(1, cliente.getCuit());
             pstmt.setString(2, cliente.getNombre());
@@ -35,7 +35,7 @@ public class ClienteJDBC implements DAOCliente {
 
     @Override
     public Cliente buscarClientePorId(int id){
-        String sql = "SELECT * FROM clientes WHERE id = ?";
+        String sql = "SELECT * FROM cliente WHERE id = ?";
         Cliente cliente = null;
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setInt(1, id);
@@ -59,33 +59,46 @@ public class ClienteJDBC implements DAOCliente {
 
     @Override
     public void agregarCliente(Cliente cliente){
-        String sql = "INSERT INTO clientes (cuit, nombre, email, direccion, coordenadas) VALUES (?, ?, ?, ?, ?)";
-        try ( PreparedStatement pstmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
-            pstmt.setString(1, cliente.getCuit());
-            pstmt.setString(2, cliente.getNombre());
-            pstmt.setString(3, cliente.getEmail());
-            pstmt.setString(4, cliente.getDireccion());
-            pstmt.setObject(5, cliente.getCoordenadas());
-            int affectedRows = pstmt.executeUpdate();
-            if (affectedRows == 0) {
-                throw new SQLException("crearCliente falla, no hay filas afectadas");
+        String sqlCoordenada = "INSERT INTO coordenada (Lat, Lng) VALUES (?, ?)";
+        String sqlCliente = "INSERT INTO cliente (CUIT, Nombre, Email, Direccion, CoordenadaID) VALUES (?, ?, ?, ?, ?)";
+        
+        try ( PreparedStatement pstmtCoordenada = connection.prepareStatement(sqlCoordenada, Statement.RETURN_GENERATED_KEYS);
+                PreparedStatement pstmtCliente = connection.prepareStatement(sqlCliente);){
+            
+            pstmtCoordenada.setDouble(1, cliente.getCoordenadas().getLat());
+            pstmtCoordenada.setDouble(2, cliente.getCoordenadas().getLng());
+            
+            int affectedRowsCoordenada = pstmtCoordenada.executeUpdate();
+            if (affectedRowsCoordenada == 0) {
+                throw new SQLException("No fue posible crear las coordenadas");
             }
-            try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+            try (ResultSet generatedKeys = pstmtCoordenada.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
-                    cliente.setId(generatedKeys.getInt(1)); 
+                    int coordenadaID = generatedKeys.getInt(1);
+                    
+                    pstmtCliente.setString(1, cliente.getCuit());
+                    pstmtCliente.setString(2, cliente.getNombre());
+                    pstmtCliente.setString(3, cliente.getEmail());
+                    pstmtCliente.setString(4, cliente.getDireccion());
+                    pstmtCliente.setObject(5, coordenadaID);
+                    
+                    int affectedRows = pstmtCliente.executeUpdate();
+                    if (affectedRows == 0) {
+                        throw new SQLException("No fue posible crear el cliente");
+                    }
                 } else {
                     throw new SQLException("crearCliente falla, no obtiene ID.");
                 }
             }
         }
         catch (SQLException e) {
-            System.err.println("Error al crear cliente por ID: " + e.getMessage());
+            System.err.println("Error al crear cliente: " + e.getMessage());
         } 
     }
     
     @Override
     public void eliminarCliente(int id){
-        String sql = "DELETE FROM clientes WHERE id = ?";
+        String sql = "DELETE FROM cliente WHERE id = ?";
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setInt(1, id);
             pstmt.executeUpdate();
@@ -97,16 +110,20 @@ public class ClienteJDBC implements DAOCliente {
     @Override
     public List<Cliente> listarClientes(){
         List<Cliente> clientes = new ArrayList<>();
-        String sql = "SELECT * FROM clientes";
+        String sql = "SELECT * FROM cliente INNER JOIN coordenada ON cliente.CoordenadaID = coordenada.ID_Coordenada";
         try (Statement stmt = connection.createStatement(); ResultSet result = stmt.executeQuery(sql)) {
             while (result.next()) {
                 Cliente cliente;
+                int coordenadaId = result.getInt("CoordenadaID");
+                double lat = result.getDouble("Lat");
+                double lng = result.getDouble("Lng");
+                Coordenada coordenadas = new Coordenada(coordenadaId, lat, lng);
                 cliente = new Cliente(
                     result.getString("cuit"),
                     result.getString("nombre"),
                     result.getString("email"),
                     result.getString("direccion"),
-                    (Coordenada) result.getObject("coordenadas")
+                    coordenadas
                 );
                 clientes.add(cliente);
             }
